680bbde7579960e734e45b36e51f07b1d7d3e2df
Sparisoma Viridi <dudung@gmail.com>
2024-05-25 19:12:00 +0700

    update

--- src/rplibs/string_list.py
+++ src/rplibs/string_list.py
@@ -8,3 +8,16 @@ def get_max_length(labels):
     n = get_lengths(labels)
     nmax = max(n)
     return nmax
+
+def print_table(type, stores, sales):
+    spaces = ' ' * get_max_length(stores) + ' '
+    print(spaces, 'Sales')
+    print('Stores', end='\t')
+    for i in type:
+        print(i, end='\t')
+    print()
+    for i in sales:
+        print(i, end='\t')
+        for j in sales[i]:
+            print(j, end='\t')
+        print()
